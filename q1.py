96711bfb8fa0fb8b0787bb11d74dd70898bc9228
Adrian Schroeter <adrian.schroeter@googlemail.com>
2012-11-14 16:54:00 -0800

    added comments and made the solution more pythony

--- q1.py
+++ q1.py
@@ -24,6 +24,12 @@
 
 import sys
 
+# function that computes the way from character to character in the movie name
+#
+# movie_name : the name of the movie to input to the tivo
+# grid_width : the width of the gird layout for the characters
+#
+# return     : the sequence to move through a movie_name
 def findSequence(movie_name, grid_width):
   current_char = 'a'
   current_sequence = ''
@@ -32,7 +38,23 @@ def findSequence(movie_name, grid_width):
     current_char = destination_char
   return current_sequence
 
+# to compute the way between two letters, we use the ascii character number and computer the rows 
+# and column index of a character by treating the grid_width as wrap around limit. 
+#
+# current_char      : the character that the cursor is currently at
+# desitination_char : the character that the cursers needs to go to
+# grid_width        : the width of the gird layout for the characters
+#
+# return            : the sequence needed to go from current to desination
 def computeWay(current_char, destination_char, grid_width):
+  # computing the cooridnates of a character is done by using the ascii value - ascii value of 'a'
+  # to compute the location of the character in an sorted array of lower case characters:
+  #
+  #  ord(current_char) - ord('a')
+  #
+  # using this location we can compute the x and y coordnate with (0,0) being in the upper left 
+  # corner by modluo and divide. The location modulo the grid width yields the column whereas
+  # dividing yields the row.
   current_x = (ord(current_char) - ord('a')) % grid_width
   current_y = (ord(current_char) - ord('a')) / grid_width
   
@@ -40,20 +62,28 @@ def computeWay(current_char, destination_char, grid_width):
   destination_y = (ord(destination_char) - ord('a')) / grid_width
 
   sequence = ''
-  while current_x - destination_x != 0 or current_y - destination_y != 0:
-    if current_x - destination_x > 0:
-      sequence += 'l'
-      current_x -= 1
-    elif current_y - destination_y > 0:
-      sequence += 'u'
-      current_y -= 1
-    elif current_x - destination_x < 0:
-      sequence += 'r'
-      current_x += 1
-    else:
-      sequence += 'd'
-      current_y += 1
+  # these if condiations compute the number of steps the cursor needs to move in each direction
+  # note that the order is important as it avoids invalid moves.
+  # For instance in the above example moving from 'x' to 'z' requires to first more left and then
+  # to move down, because there is no letter below 'x' in the grid
+  if current_x - destination_x > 0:
+    sequence += 'l' * (current_x - destination_x)
+  if current_y - destination_y > 0:
+    sequence += 'u' * (current_x - destination_x)
+  if current_x - destination_x < 0:
+    sequence += 'r' * (destination_x - current_x)
+  if current_y - destination_y < 0:
+    sequence += 'd' * (destination_y - current_y)
+  
   return sequence
+
+# additional notes and thoughts:
+#
+# as you saw we did never represent the actual grid in the program and computed the location
+# of each character on the fly. This assumes that the characters follow the order in the ascci table
+# as well as assumes that we only work with lower case letters.
+# But it is always a good idea to consider memory constrains and the necessaty of keeping the actual
+# information in memory.
 
 sequence = findSequence(sys.argv[1], int(sys.argv[2]))
 print(sequence)
